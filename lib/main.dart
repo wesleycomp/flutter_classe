62d81f16ba9e35a1d064b995ff1cc4709b329624
weslleycomp@gmail.com <weslleycomp@gmail.com>
2019-11-22 17:35:45 -0300

    add arquivosss

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       primarySwatch: Colors.red,
+     ),
+     home: MyHomePage(title: 'Exemplo Flutter'),
+   );
+ }
+}
+
+class MyHomePage extends StatefulWidget {
+ MyHomePage({Key key, this.title}) : super(key: key);
+ final String title;
+ 
+ @override
+ _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+ int _counter = 0;
+ 
+ void _incrementCounter() {
+   setState(() {
+     _counter++;
+   });
+ }
+ 
+ void _decrementCounter() {
+   setState(() {
+     _counter--;
+   });
+ }
+ 
+
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text(widget.title),
+     ),
+     body: Center(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+           Text(
+             'Contador de cliques',
+           ),
+           Text(
+             '$_counter',
+             style: Theme.of(context).textTheme.display1,
+           ),
+         ],
+       ),
+     ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: _incrementCounter,
+       tooltip: 'Increment',
+       child: Icon(Icons.add),
+     ),
+     
+   );
+ }
+}
